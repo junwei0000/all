9d13582703cabd93d0dcd1f0a59cc1affa907d47
junwei.duan <bestdo0000>
2018-01-24 11:41:20 +0800

    【项目】   kiwi 【更新】 匿名优化

---
+++ KiwiSports/src/com/KiwiSports/control/activity/UpdateInfoUtils.java
@@ -0,0 +1,118 @@
+package com.KiwiSports.control.activity;
+
+import java.util.HashMap;
+
+import com.KiwiSports.R;
+import com.KiwiSports.business.UserAccountUpdateBusiness;
+import com.KiwiSports.business.UserAccountUpdateBusiness.GetAccountUpdateCallback;
+import com.KiwiSports.utils.CommonUtils;
+import com.KiwiSports.utils.ConfigUtils;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.util.Log;
+
+public class UpdateInfoUtils {
+
+	Activity mContext;
+	private HashMap<String, String> mhashmap;
+	private ProgressDialog mDialog;
+	private SharedPreferences bestDoInfoSharedPrefs;
+	private String uid;
+	private String token;
+	private String access_token;
+	private String hobby;
+	private String nick_name;
+	private int sex;
+	private String is_anonymous;
+
+	private void showDilag() {
+		try {
+			if (mDialog == null) {
+				mDialog = CommonUtils.getInstance().createLoadingDialog(mContext);
+			} else {
+				mDialog.show();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public UpdateInfoUtils(Activity mContext) {
+		this.mContext = mContext;
+		bestDoInfoSharedPrefs = CommonUtils.getInstance().getBestDoInfoSharedPrefs(mContext);
+		uid = bestDoInfoSharedPrefs.getString("uid", "");
+		token = bestDoInfoSharedPrefs.getString("token", "");
+		access_token = bestDoInfoSharedPrefs.getString("access_token", "");
+		hobby = bestDoInfoSharedPrefs.getString("hobby", "");
+		nick_name = bestDoInfoSharedPrefs.getString("nick_name", "");
+		sex = bestDoInfoSharedPrefs.getInt("sex", 1);
+		is_anonymous = bestDoInfoSharedPrefs.getString("is_anonymous", "1");
+	}
+
+	public void UpdateInfo(String typekey, String typevalue) {
+		if (typekey.equals("hobby")) {
+			hobby = typevalue;
+		} else if (typekey.equals("nick_name")) {
+			nick_name = typevalue;
+		} else if (typekey.equals("sex")) {
+			sex = Integer.parseInt(typevalue);
+		} else if (typekey.equals("is_anonymous")) {
+			is_anonymous = typevalue;
+		}
+		processUpdateInfo(typekey,typevalue);
+	}
+
+	private void processUpdateInfo(final String typekey,final String typevalue) {
+		if (!ConfigUtils.getInstance().isNetWorkAvaiable(mContext)) {
+			CommonUtils.getInstance().initToast(mContext, mContext.getString(R.string.net_tishi));
+			return;
+		}
+		showDilag();
+		mhashmap = new HashMap<String, String>();
+		mhashmap.put("uid", uid);
+		mhashmap.put("token", token);
+		mhashmap.put("access_token", access_token);
+		mhashmap.put("hobby", hobby);
+		mhashmap.put("nick_name", nick_name + "");
+		mhashmap.put("sex", sex + "");
+		mhashmap.put("is_anonymous", is_anonymous);
+		Log.e("decrypt----", mhashmap.toString());
+		new UserAccountUpdateBusiness(mContext, "info", mhashmap, new GetAccountUpdateCallback() {
+
+			@Override
+			public void afterDataGet(HashMap<String, Object> dataMap) {
+				CommonUtils.getInstance().setOnDismissDialog(mDialog);
+				if (dataMap != null) {
+					String status = (String) dataMap.get("status");
+					if (status.equals("200")) {
+						SharedPreferences bestDoInfoSharedPrefs = CommonUtils.getInstance()
+								.getBestDoInfoSharedPrefs(mContext);
+						Editor bestDoInfoEditor = bestDoInfoSharedPrefs.edit();
+						bestDoInfoEditor.putString(typekey, typevalue);
+						bestDoInfoEditor.commit();
+						if (typekey.equals("nick_name")||typekey.equals("sex")) {
+							mContext.finish();
+						}  
+						if(!typekey.equals("is_anonymous")){
+							String msg = (String) dataMap.get("msg");
+							CommonUtils.getInstance().initToast(mContext, msg);
+						}
+					} else {
+						String msg = (String) dataMap.get("msg");
+						CommonUtils.getInstance().initToast(mContext, msg);
+					}
+				} else {
+					CommonUtils.getInstance().initToast(mContext, mContext.getString(R.string.net_tishi));
+				}
+				// 清除缓存
+				CommonUtils.getInstance().setClearCacheBackDate(mhashmap, dataMap);
+
+			}
+		});
+	}
+
+}
---
+++ KiwiSports/src/com/KiwiSports/control/adapter/VenuesTypeAdapter.java
@@ -0,0 +1,87 @@
+package com.KiwiSports.control.adapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.KiwiSports.R;
+import com.KiwiSports.model.HobbyInfo;
+import com.KiwiSports.model.VenuesListInfo;
+import com.KiwiSports.utils.LanguageUtil;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class VenuesTypeAdapter extends BaseAdapter {
+
+	private ArrayList<HobbyInfo> list;
+	private Activity context;
+	String select;
+
+	public VenuesTypeAdapter(Activity context, String select, ArrayList<HobbyInfo> list) {
+		super();
+		this.context = context;
+		this.list = list;
+		this.select = select;
+	}
+
+	public String getSelect() {
+		return select;
+	}
+
+	public void setSelect(String select) {
+		this.select = select;
+	}
+
+	@Override
+	public int getCount() {
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		HobbyInfo stadiumObj = (HobbyInfo) list.get(position);
+		final ViewHolder viewHolder;
+		if (convertView == null) {
+			convertView = LayoutInflater.from(context).inflate(R.layout.venues_hobby_item, null);
+			viewHolder = new ViewHolder();
+
+			viewHolder.useraccount_sex_tv_famale = (TextView) convertView.findViewById(R.id.useraccount_sex_tv_famale);
+			viewHolder.useraccount_sex_iv_famale = (ImageView) convertView.findViewById(R.id.useraccount_sex_iv_famale);
+			convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
+		String Ehobby = stadiumObj.getEhobby();
+		String zhobby = stadiumObj.getZhobby();
+		if (!LanguageUtil.idChLanguage(context)) {
+			zhobby = Ehobby;
+		}
+		viewHolder.useraccount_sex_tv_famale.setText(zhobby);
+		if (select.equals(Ehobby)) {
+			viewHolder.useraccount_sex_iv_famale.setVisibility(View.VISIBLE);
+		} else {
+			viewHolder.useraccount_sex_iv_famale.setVisibility(View.INVISIBLE);
+		}
+		return convertView;
+	}
+
+	class ViewHolder {
+		public TextView useraccount_sex_tv_famale;
+		public ImageView useraccount_sex_iv_famale;
+	}
+}
