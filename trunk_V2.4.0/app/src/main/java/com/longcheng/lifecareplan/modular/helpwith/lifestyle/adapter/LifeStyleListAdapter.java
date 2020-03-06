ae18559b92f9b7089b036fe006491c27c5c69b5f
duanjunwei <>
2020-03-06 19:56:38 +0800

    【版本：V2.6.3】更新内容：商城  生活方式

--- trunk_V2.4.0/app/src/main/java/com/longcheng/lifecareplan/modular/helpwith/lifestyle/adapter/LifeStyleListAdapter.java
+++ trunk_V2.4.0/app/src/main/java/com/longcheng/lifecareplan/modular/helpwith/lifestyle/adapter/LifeStyleListAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.longcheng.lifecareplan.R;
@@ -49,102 +48,30 @@ public class LifeStyleListAdapter extends BaseAdapterHelper<LifeStyleItemBean> {
         LifeStyleItemBean mHelpItemBean = list.get(position);
         String groupimg = mHelpItemBean.getGroup_img();
         String gs_name = mHelpItemBean.getReceive_group_name();
-        int my_bless = mHelpItemBean.getMy_bless();
-        int bless_me = mHelpItemBean.getBless_me();
         String action_image = mHelpItemBean.getGoods_img();
         String action_name = mHelpItemBean.getGoods_name();
         String h_user = mHelpItemBean.getReceive_user_name();
-        String skb_price_action = mHelpItemBean.getSkb_cumulative_price();
-        String super_ability_cumulative_price = mHelpItemBean.getSuper_ability_cumulative_price();
         String date = mHelpItemBean.getDate();
         mHolder.item_tv_date.setText(date);
         GlideDownLoadImage.getInstance().loadCircleImageCommune(context, groupimg, mHolder.item_iv_communethumb);
         GlideDownLoadImage.getInstance().loadCircleImageRoleGoods(context, action_image, mHolder.item_iv_thumb, 0);
         mHolder.item_iv_communename.setText(gs_name);
-        if (my_bless == 0) {
-            mHolder.item_iv_helpother.setVisibility(View.GONE);
-            mHolder.item_tv_helpother.setVisibility(View.GONE);
-        } else {
-            mHolder.item_iv_helpother.setVisibility(View.VISIBLE);
-            mHolder.item_tv_helpother.setVisibility(View.VISIBLE);
-            mHolder.item_tv_helpother.setText("我祝福" + my_bless + "次");
-        }
-        if (bless_me == 0) {
-            mHolder.item_iv_helpme.setVisibility(View.GONE);
-            mHolder.item_tv_helpme.setVisibility(View.GONE);
-        } else {
-            mHolder.item_iv_helpme.setVisibility(View.VISIBLE);
-            mHolder.item_tv_helpme.setVisibility(View.VISIBLE);
-            mHolder.item_tv_helpme.setText("祝福我" + bless_me + "次");
-        }
         mHolder.item_tv_content.setText(action_name);
         mHolder.item_tv_name.setText("接福人：" + h_user);
 
-
-        String showcn = " | 已有" + super_ability_cumulative_price + "超能";
-        mHolder.item_tv_lifeenergynum.setText(showcn);
-        String showT = " | 已有" + skb_price_action + "寿康宝";
-        mHolder.item_tv_lifeskbnum.setText(showT);
-
-
-        int buy_type = mHelpItemBean.getBuy_type();
-        if (buy_type == 2) {
-            mHolder.item_tv_lifeenergynum.setVisibility(View.VISIBLE);
-            mHolder.tv_jiange.setVisibility(View.GONE);
-            mHolder.item_tv_lifeskbnum.setVisibility(View.GONE);
-            mHolder.relat_cn.setVisibility(View.VISIBLE);
-            mHolder.relat_skb.setVisibility(View.GONE);
-
-            int super_ability_progress = mHelpItemBean.getSuper_ability_progress();
-            mHolder.pb_lifeenergynum.setProgress(super_ability_progress);
-            mProgressUtils.showNum(super_ability_progress, mHolder.pb_lifeenergynum, mHolder.item_pb_numne);
-            mHolder.pb_lifeenergynum.setReachedBarColor(context.getResources().getColor(R.color.engry_btn_bg));
-            ColorChangeByTime.getInstance().changeDrawableToClolor(context, mHolder.item_pb_numne, R.color.engry_btn_bg);
-        } else if (buy_type == 3) {
-            mHolder.item_tv_lifeenergynum.setVisibility(View.VISIBLE);
-            mHolder.tv_jiange.setVisibility(View.VISIBLE);
-            mHolder.item_tv_lifeskbnum.setVisibility(View.VISIBLE);
-            mHolder.relat_cn.setVisibility(View.VISIBLE);
-            mHolder.relat_skb.setVisibility(View.VISIBLE);
-
-
-            int super_ability_progress = mHelpItemBean.getSuper_ability_progress();
-            mHolder.pb_lifeenergynum.setProgress(super_ability_progress);
-            mProgressUtils.showNum(super_ability_progress, mHolder.pb_lifeenergynum, mHolder.item_pb_numne);
-            mHolder.pb_lifeenergynum.setReachedBarColor(context.getResources().getColor(R.color.engry_btn_bg));
-            ColorChangeByTime.getInstance().changeDrawableToClolor(context, mHolder.item_pb_numne, R.color.engry_btn_bg);
-
-            int progress = mHelpItemBean.getProgress();
-            mHolder.item_pb_lifeskbnum.setProgress(progress);
-            mProgressUtils.showNum(progress, mHolder.item_pb_lifeskbnum, mHolder.item_pb_numskb);
-            mHolder.item_pb_lifeskbnum.setReachedBarColor(context.getResources().getColor(R.color.red));
-            ColorChangeByTime.getInstance().changeDrawableToClolor(context, mHolder.item_pb_numskb, R.color.red);
-
-        } else {
-            mHolder.item_tv_lifeenergynum.setVisibility(View.GONE);
-            mHolder.tv_jiange.setVisibility(View.GONE);
-            mHolder.item_tv_lifeskbnum.setVisibility(View.VISIBLE);
-            mHolder.relat_cn.setVisibility(View.GONE);
-            mHolder.relat_skb.setVisibility(View.VISIBLE);
-
-            int progress = mHelpItemBean.getProgress();
-            mHolder.item_pb_lifeskbnum.setProgress(progress);
-            mProgressUtils.showNum(progress, mHolder.item_pb_lifeskbnum, mHolder.item_pb_numskb);
-            mHolder.item_pb_lifeskbnum.setReachedBarColor(context.getResources().getColor(R.color.red));
-            ColorChangeByTime.getInstance().changeDrawableToClolor(context, mHolder.item_pb_numskb, R.color.red);
-        }
+        int super_ability_progress = mHelpItemBean.getSuper_ability_progress();
+        mHolder.pb_lifeenergynum.setProgress(super_ability_progress);
+        mProgressUtils.showNum(super_ability_progress, mHolder.pb_lifeenergynum, mHolder.item_pb_numne);
+        mHolder.pb_lifeenergynum.setReachedBarColor(context.getResources().getColor(R.color.engry_btn_bg));
+        ColorChangeByTime.getInstance().changeDrawableToClolor(context, mHolder.item_pb_numne, R.color.engry_btn_bg);
         return convertView;
     }
 
     private class ViewHolder {
         private ImageView item_iv_communethumb;
         private TextView item_iv_communename;
+        private TextView item_tv_date;
 
-        private ImageView item_iv_helpother;
-        private TextView item_tv_helpother;
-
-        private ImageView item_iv_helpme;
-        private TextView item_tv_helpme;
 
         private ImageView item_iv_thumb;
         private TextView item_tv_content;
@@ -152,38 +79,17 @@ public class LifeStyleListAdapter extends BaseAdapterHelper<LifeStyleItemBean> {
 
         private NumberProgressBar pb_lifeenergynum;
         private TextView item_pb_numne;
-        private NumberProgressBar item_pb_lifeskbnum;
-        private TextView item_pb_numskb;
-        private TextView item_tv_date;
-        private TextView item_tv_lifeenergynum;
-        private TextView item_tv_lifeskbnum;
-
-        private RelativeLayout relat_cn;
-        private RelativeLayout relat_skb;
-        private TextView tv_jiange;
 
 
         public ViewHolder(View view) {
-            tv_jiange = (TextView) view.findViewById(R.id.tv_jiange);
-            relat_cn = (RelativeLayout) view.findViewById(R.id.relat_cn);
-            relat_skb = (RelativeLayout) view.findViewById(R.id.relat_skb);
             item_iv_communethumb = (ImageView) view.findViewById(R.id.item_iv_communethumb);
             item_iv_communename = (TextView) view.findViewById(R.id.item_iv_communename);
-            item_iv_helpother = (ImageView) view.findViewById(R.id.item_iv_helpother);
-            item_tv_helpother = (TextView) view.findViewById(R.id.item_tv_helpother);
-            item_iv_helpme = (ImageView) view.findViewById(R.id.item_iv_helpme);
-            item_tv_helpme = (TextView) view.findViewById(R.id.item_tv_helpme);
-
             item_iv_thumb = (ImageView) view.findViewById(R.id.item_iv_thumb);
             item_tv_content = (TextView) view.findViewById(R.id.item_tv_content);
             item_tv_name = (TextView) view.findViewById(R.id.item_tv_name);
 
             pb_lifeenergynum = (NumberProgressBar) view.findViewById(R.id.item_pb_lifeenergynum);
             item_pb_numne = (TextView) view.findViewById(R.id.item_pb_numne);
-            item_pb_lifeskbnum = (NumberProgressBar) view.findViewById(R.id.item_pb_lifeskbnum);
-            item_pb_numskb = (TextView) view.findViewById(R.id.item_pb_numskb);
-            item_tv_lifeenergynum = (TextView) view.findViewById(R.id.item_tv_lifeenergynum);
-            item_tv_lifeskbnum = (TextView) view.findViewById(R.id.item_tv_lifeskbnum);
             item_tv_date = (TextView) view.findViewById(R.id.item_tv_date);
         }
     }
